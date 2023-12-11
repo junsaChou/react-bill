af454efafe83ffe2064019385a3f18518f02c147
junsa <junsa163@163.com>
2023-12-11 18:02:52 +0800

    单日列表账单渲染

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import { useMemo } from "react";
 import './index.scss'
+import { billTypeToName } from '@/contants'
 
 const DayBill = ({date,billList})=>{
     const dayResult = useMemo(()=>{
@@ -35,6 +36,25 @@ const DayBill = ({date,billList})=>{
                         <span className="type">结余</span>
                     </div>
                 </div>
+            </div>
+            {/* 单日列表 */}
+            <div className="billList">
+                {
+                    billList.map(item=>{
+                        return(
+                            <div className="bill" key={item.id}>
+                                <div className="detail">
+                                    <div className="billType">
+                                        { billTypeToName[item.useFor]}
+                                    </div>
+                                </div>
+                                <div className={classNames('money',item.type)}>
+                                    {item.money.toFixed(2)}
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
