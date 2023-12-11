ca6a77412fdc2306daf4c8670e64e06d4e43bb22
junsa <junsa163@163.com>
2023-12-11 12:10:25 +0800

    月度账单 时间显示 dayjs

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,26 +1,56 @@
-import { Outlet} from 'react-router-dom'
-import { Button} from 'antd-mobile'
+import {  TabBar } from 'antd-mobile'
 import { useEffect } from 'react'
+import { Outlet,useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { getBillList } from '@/store/modules/billStore'
+import './index.scss'
+import {
+ BillOutline,
+ CalculatorOutline,
+ AddCircleOutline
+} from 'antd-mobile-icons'
+
+  const tabs = [
+    {
+      key: '/month',
+      title: '月度账单',
+      icon: <BillOutline />
+    },
+    {
+      key: '/new',
+      title: '记账',
+      icon: <AddCircleOutline />,
+    },
+    {
+      key: '/year',
+      title: '年度账单',
+      icon: <CalculatorOutline />
+    }
+  ]
 const Layout = () =>{
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getBillList())
+  },[dispatch])
+  const navigate = useNavigate();
+  const switchRoute = (path)=>{
+    console.log(path)
+    navigate(path)
+  }
 
-    useEffect(()=>{
-        dispatch(getBillList())
-    },[dispatch])
-    return(
-        <div>
+  return (
+    <div className='layout'>
+        <div className='container'>
             <Outlet/>
-             我是layout组件
-            {/* 测试全局组件是否生效 */}
-            <Button color='primary'>测试全局</Button>
-                <div className='pupple'> 
-                    <Button color='primary'>测试局部</Button>
-            </div>
         </div>
-    )
-
+        <div className='footer'>
+            <TabBar onChange={switchRoute}>
+            {tabs.map(item => (
+                <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+            ))}
+            </TabBar>
+        </div>
+    </div>
+  )
 }
-
 export default Layout
--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,5 +1,84 @@
+import { NavBar,DatePicker} from 'antd-mobile'
+import { useState } from 'react'
+import classNames from 'classnames'
+import dayjs from 'dayjs'
+import './index.scss'
+
 const Month = () =>{
-    return <div> 我是Month组件</div>
+    // 控制弹窗的打开和关闭
+    const [ dateVisible,setDateVisible ] = useState(false)
+
+    // 控制时间显示
+    const [ currentDate,setCurrentDate ] = useState(()=>{
+        return  dayjs( new Date()).format('YYYY-MM')
+    })
+
+    const onConfirm = (date) =>{
+        setDateVisible(false)
+        // 其他逻辑
+
+        console.log( date)
+        const formatDate = dayjs( date).format('YYYY-MM')
+        setCurrentDate(formatDate)
+    }
+
+    return (
+        <div className='monthLyBill'>
+            <NavBar className='nav' backArrow={false}>
+                月度收支
+            </NavBar>
+            <div className='content'>
+                <div className='header'>
+                    {/* 时间切换区域 */}
+                    <div className='date' onClick={ ()=>setDateVisible(true)}>
+                        <span className='text'>
+                            {currentDate + ''}月账单
+                        </span>
+                        {/* 根据弹窗状态 控制 朝上 或者朝下 */}
+                        <span className= {classNames( 'arrow', dateVisible && 'expand') }></span>
+                    </div>
+                    {/* 统计区域 */}
+                    <div className='twoLineOverview'>
+                        <div className='item'>
+                            <div className='money'>
+                                { 100 }
+                            </div>
+                            <div className='type'>
+                                支出
+                            </div>
+                        </div>
+                        <div className='item'>
+                            <div className='money'>
+                                { 200 }
+                            </div>
+                            <div className='type'>
+                                收入
+                            </div>
+                        </div>
+                        <div className='item'>
+                            <div className='money'>
+                                { 100 }
+                            </div>
+                            <div className='type'>
+                                结余
+                            </div>
+                        </div>
+                        {/*时间选择器 */}
+                        <DatePicker
+                            className='kaDate'
+                            title='记账日期'
+                            precision='month'
+                            visible={dateVisible}
+                            onCancel={()=>setDateVisible(false)}
+                            onConfirm={onConfirm}
+                            onClose={()=>setDateVisible(false)}
+                            max={ new Date()}
+                            />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Month
