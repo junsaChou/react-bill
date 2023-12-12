5ad77e6dd96148cfd0177103c8017e31a789c438
junsa <junsa163@163.com>
2023-12-12 12:18:39 +0800

    新增一个账单 收尾

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addBillList } from '@/store/modules/billStore'
+import dayjs from 'dayjs'
 const New = () =>{
     const navigate = useNavigate()
     // 1.z准备 控制收入 支出的状态
@@ -25,11 +26,21 @@ const New = () =>{
         const data = {
             type: billType,
             money: billType === 'pay' ? -money : +money,
-            date: new Date(),
+            date: date,
             useFor: useFor
         }
         console.log( data)
         dispatch( addBillList(data) )
+    }
+    // 存储选择时间
+    const [date,setDate] = useState(new Date())
+    // 控制时间打开和关闭
+    const [dateVisible,setDateVisible ] = useState(false);
+    // 确认选择时间
+    const dateConfirm = (value)=>{
+        console.log( value)
+        setDate(value)
+        setDateVisible(false)
     }
     return (
         <div className='keepAccounts'>
@@ -49,11 +60,14 @@ const New = () =>{
                     <div className='kaForm'>
                         <div className='date'>
                             <Icon type='calendar'  className='icon' />
-                            <span className='text'>{'今天'}</span>
+                            <span className='text' onClick={ ()=> setDateVisible(true) } >{dayjs(date).format('YYYY-MM-DD')}</span>
+                            {/* 时间选择器 */}
                             <DatePicker
                                 className='kaDate'
                                 title='记账日期'
                                 max={ new Date()}
+                                visible={ dateVisible}
+                                onConfirm={dateConfirm}
                             />
                         </div>
                         <div className='kaInput'>
@@ -80,7 +94,8 @@ const New = () =>{
                                 <div className='list' >
                                     { item.list.map(item=>{
                                         return (
-                                            <div className={ classNames( 'item','' ) } key={item.type} onClick={ ()=> setUseFor(item.type) } >
+                                            // selected 激活状态
+                                            <div className={ classNames( 'item', useFor === item.type ? 'selected' :'' ) } key={item.type} onClick={ ()=> setUseFor(item.type) } >
                                                 <div className='icon' >
                                                    <Icon type={item.type}/>
                                                 </div>
